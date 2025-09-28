d61c8f97040ffecdfda5457d4c7ec3aad01d1616
codev911 <codev911@gmail.com>
2025-09-28 16:24:55 +0700

    fix: eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -81,6 +81,8 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'no-console': 'off',
     },
